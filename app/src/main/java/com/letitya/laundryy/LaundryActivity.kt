b71ec3848880543c2a16869189abf1b0ffd5973f
Kirtdee <letityatsaqifa12@gmail.com>
2025-01-10 13:35:48 +0700

    menambahkan kotlin untuk tanggal

--- app/src/main/java/com/letitya/laundryy/LaundryActivity.kt
+++ app/src/main/java/com/letitya/laundryy/LaundryActivity.kt
@@ -5,12 +5,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class LaundryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_laundry)
+        val textGreeting: TextView = findViewById(R.id.tvSelamat)
+        val textDate: TextView = findViewById(R.id.tvTgl)
+
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+
+        val greeting = when {
+            hour in 5..11 -> "Selamat Pagi, Letitya"
+            hour in 12..14 -> "Selamat Siang, Letitya"
+            hour in 15..17 -> "Selamat Sore, Letitya"
+            else -> "Selamat Malam, Letitya"
+        }
+
+        textGreeting.text = greeting
+
+        val dateFormat = SimpleDateFormat("dd MMMM yyyy", Locale("id", "ID"))
+        val currentDate = dateFormat.format(calendar.time)
+
+        textDate.text = currentDate
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
